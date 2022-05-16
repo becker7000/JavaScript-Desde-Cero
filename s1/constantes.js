c5c09e1d2bf12cc4c3939c3975b8484cf56f7717
Erick Alvarez <becker7000@outlook.es>
2022-05-16 13:00:33 -0500

    variables

---
+++ s1/constantes.js
@@ -0,0 +1,4 @@
+
+
+const PI = 3.1415926535;
+console.log("\n\t El valor de PI es: %f",PI);
---
+++ s1/variables.js
@@ -0,0 +1,26 @@
+
+
+var numUno=7;
+
+//var 3numero; //Esto está mal.
+var numero3; //Esto está correcto.
+
+/**
+ * Estilos de escritura de variables (naming en JavaScript)
+ * 
+ * Camell case: numUno (Lower Camell Case)
+ * Pascal case: NumUno (Upper Camell Case)
+ * Snake case: num_uno
+ * 
+ */
+
+
+var var_uno=12;
+
+
+//No podemos redeclarar con let
+let numero=5;
+// let numero=6; <- esta línea daría un error.
+
+var x=10;
+var x=20;
