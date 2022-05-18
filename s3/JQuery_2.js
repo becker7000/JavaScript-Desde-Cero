b9eacc7ca50aecfc72093031bcf604891cf968f6
Erick Alvarez <becker7000@outlook.es>
2022-05-18 15:21:11 -0500

    mostrando usuarios

--- s3/JQuery_2.js
+++ s3/JQuery_2.js
@@ -3,11 +3,25 @@ $.ajax("https://randomuser.me/api?results=10",{
 
     success(json){
         for(let user of json.results){
-            console.log("\n\t %s",user.name.title);
-            console.log("\n\t Name: %s",user.name.first);
-            console.log("\n\t Phone: %s",user.phone);
-            console.log("\n\t Age: %d",user.dob.age);
-            console.log("\n\t-----------------------");
+            // console.log("\n\t %s",user.name.title);
+            // console.log("\n\t Name: %s",user.name.first);
+            // console.log("\n\t Phone: %s",user.phone);
+            // console.log("\n\t Age: %d",user.dob.age);
+            // console.log("\n\t-----------------------");
+            
+            $("#usuarios").append(
+                $("<li></li>").html(`
+                    <div>
+                        <img src="${user.picture.thumbnail}" alt="user">
+                        <br>
+                        <span>${user.name.title}</span><br>
+                        <span>Name:${user.name.first}</span><br>
+                        <span>Phone:${user.phone}</span><br>
+                        <span>Age:${user.dob.age} </span>
+                    </div>
+                `)
+            )
+
         }
     },
     error(error){
