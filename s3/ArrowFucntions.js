770a8c5a6b0ee15a373b0d7396ef8687c83fabbc
Erick Alvarez <42850106+becker7000@users.noreply.github.com>
2025-09-02 04:04:32 -0600

    Delete s3 directory

--- s3/ArrowFucntions.js
+++
@@ -1,5 +0,0 @@
-
-// Arrow functions: son funciones anónimas:
-// Novedad de ES6:
-const cuadrado = (n) => n**2;
-console.log("Cuadrado: %d",cuadrado(7));
--- s3/cookie.js
+++
@@ -1,4 +0,0 @@
-
-function crearCookie(){
-    document.cookie="nombre="+document.forms["formulario"]["nombre"].value;
-}
